c1eeea7a6db08262677203e60615ff7b5be23499
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-14 09:37:26 +0300

    Update admin.py

--- provider/admin.py
+++ provider/admin.py
@@ -5,4 +5,4 @@ from .models import ProviderProduct
 @admin.register(ProviderProduct)
 class ProviderProductAdmin(admin.ModelAdmin):
     list_display = ['name', ]
-    exclude = ('author', 'booking', 'count', 'moderation', 'up_price', 'day_next_publish', 'name_tmp', 'device_provider')
+    exclude = ('booking', 'count', 'moderation', 'up_price', 'day_next_publish', 'name_tmp', 'device_provider')
