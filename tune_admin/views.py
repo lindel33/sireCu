12131c521367554106736e49374459bc70cb6ff1
Сергей Островский <81719949+lindel33@users.noreply.github.com>
2022-04-16 04:57:50 +0300

    Update views.py

--- tune_admin/views.py
+++ tune_admin/views.py
@@ -141,7 +141,18 @@ all_products = [x for x in get_all_products()]
 current_product = get_current_product()
 max_products = [x for x in max_all_products()]
 
+def update_products():
+    global current_category
+    global all_products
+    global current_product
+    global max_products
 
+    current_category = list(set([x[1] for x in get_current_product()]))
+    all_products = [x for x in get_all_products()]
+    current_product = get_current_product()
+    max_products = [x for x in max_all_products()]
+    
+  
 @client.message_handler(func=lambda message: message.text == 'Запуск')
 @client.message_handler(func=lambda message: message.text == 'Начало')
 @client.message_handler(func=lambda message: message.text == 'Запустить бота')
